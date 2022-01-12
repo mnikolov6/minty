a53f154d7f532069e5b5dc817d082ceff0961f1a
mirolav <mn04905228@edu1.pgzaimov.com>
2022-01-12 09:33:30 +0200

    Dobaven metod za izvajdane

--- test1/test1/Program.cs
+++ test1/test1/Program.cs
@@ -12,6 +12,7 @@ namespace test1
         {
             Write();
             Sum();
+            Sub();
         
         }
         static void Write()
@@ -28,6 +29,19 @@ namespace test1
             Console.WriteLine();
             Console.Write("Result: ");
             Console.WriteLine(num+num2);
+
+
+        }
+        static void Sub()
+        {
+            Console.Write("Num1: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Num2: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Result: ");
+            Console.WriteLine(num - num2);
 
 
         }
