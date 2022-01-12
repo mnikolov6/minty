21984f14974c3f5c456d699799f447ff5f3b3a42
mirolav <mn04905228@edu1.pgzaimov.com>
2022-01-12 09:31:40 +0200

    Dobaven metod za subirane

--- test1/test1/Program.cs
+++ test1/test1/Program.cs
@@ -11,10 +11,25 @@ namespace test1
         static void Main(string[] args)
         {
             Write();
+            Sum();
+        
         }
         static void Write()
         {
             Console.WriteLine("Hello world!!");
+        }
+        static void Sum()
+        {
+            Console.Write("Num1: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Num2: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Result: ");
+            Console.WriteLine(num+num2);
+
+
         }
     }
 }
